39c956530263bd24862cafafa2543beaeecbf45b
Jonathan Sivahera <jsivahera@icloud.com>
2022-10-01 17:37:27 +0200

    Setup redux && add additional deps

---
+++ app/javascript/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import Greeting from "./components/Greeting";
+
+const App = () => (
+  <h1>Hello World Mod!</h1>
+  // <Routes>
+  //   <Route path="/" element={<Greeting />} />
+  // </Routes>
+);
+
+export default App;
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import store from "./redux/configureStore";
 
-function App() {
-  return <h1>Hello World!</h1>;
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>
+);
